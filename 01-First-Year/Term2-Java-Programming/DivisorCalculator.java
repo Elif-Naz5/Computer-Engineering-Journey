85d8fa1d1e8dac337080857844c8f4bc60cb6d5b
Elif Naz Ozturk <elifnazozturk314@gmail.com>
2026-08-13 15:27:14 +0300

    Add the exact divisor calculator

---
+++ 01-First-Year/Term2-Java-Programming/DivisorCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class tamBolenBulma {
+    public static void main(String[] args) {
+
+        Scanner giris= new Scanner(System.in);
+
+        int sayi,adet=0;
+
+        System.out.print("Sayı giriniz: ");
+        sayi=giris.nextInt();
+
+        for(int i=1;i<=sayi;i++){
+
+            if(sayi%i==0){
+                System.out.println(i);
+                adet++;
+            }
+        }
+
+        giris.close();
+
+        System.out.println();
+        System.out.println(adet+" tane tam böleni vardır"); 
+    }
+}
